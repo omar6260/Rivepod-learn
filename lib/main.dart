89734f7853a824a42c6cfdd79e9b7aded90f4525
Oumar Fall <foumar404@gmail.com>
2022-10-19 23:12:20 +0100

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'main.g.dart';
+
+@riverpod
+String helloWorld(HelloWorldRef ref) {
+  return 'Hello world';
+}
+
+
+void main() {
+  runApp(ProviderScope(child: MyApp()));
+}
+
+
+class MyApp extends ConsumerWidget {
+  
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final String value = ref.watch(helloWorldProvider);
+
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('First App With Rivepod'),
+        
+        ),
+        body: Center(
+          child: Text(value),
+        ),
+      ),
+    );
+  }
+}
